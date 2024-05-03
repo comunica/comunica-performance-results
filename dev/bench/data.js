46226c1afcedc3a4b7f1190d6c3c73058ae642dd
github-action-benchmark <github@users.noreply.github.com>
2024-05-03 18:14:16 +0000

    add WatDiv File (customSmallerIsBetter) benchmark result for 3e0beb77fc821a8a184168b38d93b4a8aab939db

---
+++ dev/bench/data.js
@@ -0,0 +1,62 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1714760056201,
+  "repoUrl": "https://github.com/comunica/comunica",
+  "entries": {
+    "WatDiv File": [
+      {
+        "commit": {
+          "author": {
+            "email": "ruben.taelman@ugent.be",
+            "name": "Ruben Taelman",
+            "username": "rubensworks"
+          },
+          "committer": {
+            "email": "ruben.taelman@ugent.be",
+            "name": "Ruben Taelman",
+            "username": "rubensworks"
+          },
+          "distinct": true,
+          "id": "3e0beb77fc821a8a184168b38d93b4a8aab939db",
+          "message": "Add WatDiv file performance benchmark to CI",
+          "timestamp": "2024-05-03T20:11:37+02:00",
+          "tree_id": "1dcc21d642f57d8fded1c1f9941d9d4069b0d4e9",
+          "url": "https://github.com/comunica/comunica/commit/3e0beb77fc821a8a184168b38d93b4a8aab939db"
+        },
+        "date": 1714760055755,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "WatDiv - C1.0",
+            "value": 0,
+            "unit": "ms",
+            "extra": "Results: 0; Error: false; HTTP Requests: 0"
+          },
+          {
+            "name": "WatDiv - C1.1",
+            "value": 0,
+            "unit": "ms",
+            "extra": "Results: 0; Error: false; HTTP Requests: 0"
+          },
+          {
+            "name": "WatDiv - C1.2",
+            "value": 0,
+            "unit": "ms",
+            "extra": "Results: 0; Error: false; HTTP Requests: 0"
+          },
+          {
+            "name": "WatDiv - C1.3",
+            "value": 0,
+            "unit": "ms",
+            "extra": "Results: 0; Error: false; HTTP Requests: 0"
+          },
+          {
+            "name": "WatDiv - C1.4",
+            "value": 0,
+            "unit": "ms",
+            "extra": "Results: 0; Error: false; HTTP Requests: 0"
+          }
+        ]
+      }
+    ]
+  }
+}
