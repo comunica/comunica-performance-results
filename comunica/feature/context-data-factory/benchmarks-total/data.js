164baa8b0b2d61b710115fa1e35dc3241c419e1a
github-action-benchmark <github@users.noreply.github.com>
2024-05-18 15:36:45 +0000

    add Benchmarks total results (customSmallerIsBetter) benchmark result for 48749968feff033b9e75b373142d5df9156d5d92

---
+++ comunica/feature/context-data-factory/benchmarks-total/data.js
@@ -0,0 +1,37 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1716046605698,
+  "repoUrl": "https://github.com/comunica/comunica",
+  "entries": {
+    "Benchmarks total results": [
+      {
+        "commit": {
+          "author": {
+            "email": "ruben.taelman@ugent.be",
+            "name": "Ruben Taelman",
+            "username": "rubensworks"
+          },
+          "committer": {
+            "email": "ruben.taelman@ugent.be",
+            "name": "Ruben Taelman",
+            "username": "rubensworks"
+          },
+          "distinct": true,
+          "id": "48749968feff033b9e75b373142d5df9156d5d92",
+          "message": "Allow plugging in custom DataFactory through context, Closes #358",
+          "timestamp": "2024-05-18T17:26:54+02:00",
+          "tree_id": "af3a5cb5e55c7ee4e20a0692d3fe75d5adf757c1",
+          "url": "https://github.com/comunica/comunica/commit/48749968feff033b9e75b373142d5df9156d5d92"
+        },
+        "date": 1716046605516,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "WatDiv-File",
+            "value": 7708,
+            "unit": "ms"
+          }
+        ]
+      }
+    ]
+  }
+}
