11861cfaf80199cbec0a6feba98a86ca20aedb16
github-action-benchmark <github@users.noreply.github.com>
2024-05-19 04:35:41 +0000

    add Benchmarks total results (customSmallerIsBetter) benchmark result for 1a51623fd51e59ef0ea5be21587e464c3dd4557d

---
+++ comunica/next/major/benchmarks-total/data.js
@@ -0,0 +1,37 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1716093341430,
+  "repoUrl": "https://github.com/comunica/comunica",
+  "entries": {
+    "Benchmarks total results": [
+      {
+        "commit": {
+          "author": {
+            "email": "ruben.taelman@ugent.be",
+            "name": "Ruben Taelman",
+            "username": "rubensworks"
+          },
+          "committer": {
+            "email": "ruben.taelman@ugent.be",
+            "name": "Ruben Taelman",
+            "username": "rubensworks"
+          },
+          "distinct": true,
+          "id": "1a51623fd51e59ef0ea5be21587e464c3dd4557d",
+          "message": "Merge branch 'master' into next/major",
+          "timestamp": "2024-05-19T06:23:21+02:00",
+          "tree_id": "5f1d9209437f66f3121f3f522c3a11a81fb84027",
+          "url": "https://github.com/comunica/comunica/commit/1a51623fd51e59ef0ea5be21587e464c3dd4557d"
+        },
+        "date": 1716093341271,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "WatDiv-File",
+            "value": 7784,
+            "unit": "ms"
+          }
+        ]
+      }
+    ]
+  }
+}
