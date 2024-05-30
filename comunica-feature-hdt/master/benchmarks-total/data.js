f6161db528b760553964213f345c064f444b28f1
github-action-benchmark <github@users.noreply.github.com>
2024-05-30 12:57:26 +0000

    add Benchmarks total results (customSmallerIsBetter) benchmark result for 96dddd8d2703135cf0c6d5c2d5b76e3ef528b146

---
+++ comunica-feature-hdt/master/benchmarks-total/data.js
@@ -0,0 +1,37 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1717073846088,
+  "repoUrl": "https://github.com/comunica/comunica-feature-hdt",
+  "entries": {
+    "Benchmarks total results": [
+      {
+        "commit": {
+          "author": {
+            "email": "ruben.taelman@ugent.be",
+            "name": "Ruben Taelman",
+            "username": "rubensworks"
+          },
+          "committer": {
+            "email": "ruben.taelman@ugent.be",
+            "name": "Ruben Taelman",
+            "username": "rubensworks"
+          },
+          "distinct": true,
+          "id": "96dddd8d2703135cf0c6d5c2d5b76e3ef528b146",
+          "message": "Add WatDiv performance benchmark to CI",
+          "timestamp": "2024-05-30T14:49:40+02:00",
+          "tree_id": "a79114f72b2a8ecdf0a0f1295872465ad7ab2fd4",
+          "url": "https://github.com/comunica/comunica-feature-hdt/commit/96dddd8d2703135cf0c6d5c2d5b76e3ef528b146"
+        },
+        "date": 1717073845872,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "WatDiv-HDT",
+            "value": 6387,
+            "unit": "ms"
+          }
+        ]
+      }
+    ]
+  }
+}
