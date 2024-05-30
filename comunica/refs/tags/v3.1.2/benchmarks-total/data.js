afb729021039bd734e08a3e72d6d8c9ecbf53fd8
github-action-benchmark <github@users.noreply.github.com>
2024-05-30 14:23:34 +0000

    add Benchmarks total results (customSmallerIsBetter) benchmark result for db31677bdef5400194783048840c147e04b793e5

---
+++ comunica/refs/tags/v3.1.2/benchmarks-total/data.js
@@ -0,0 +1,42 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1717079014165,
+  "repoUrl": "https://github.com/comunica/comunica",
+  "entries": {
+    "Benchmarks total results": [
+      {
+        "commit": {
+          "author": {
+            "email": "ruben.taelman@ugent.be",
+            "name": "Ruben Taelman",
+            "username": "rubensworks"
+          },
+          "committer": {
+            "email": "ruben.taelman@ugent.be",
+            "name": "Ruben Taelman",
+            "username": "rubensworks"
+          },
+          "distinct": true,
+          "id": "db31677bdef5400194783048840c147e04b793e5",
+          "message": "Bump to release version v3.1.2",
+          "timestamp": "2024-05-30T16:05:04+02:00",
+          "tree_id": "7e957d94f350c46e40ebadba3716e68df0e5360a",
+          "url": "https://github.com/comunica/comunica/commit/db31677bdef5400194783048840c147e04b793e5"
+        },
+        "date": 1717079013615,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "WatDiv-File",
+            "value": 7409,
+            "unit": "ms"
+          },
+          {
+            "name": "WatDiv-TPF",
+            "value": 43167,
+            "unit": "ms"
+          }
+        ]
+      }
+    ]
+  }
+}
