91d9f94a9db0f4c6db4af2212bf0a04aff9a2656
github-action-benchmark <github@users.noreply.github.com>
2024-05-30 10:20:25 +0000

    add Benchmarks total results (customSmallerIsBetter) benchmark result for 542ae91daa2ccc176a4ff892989e7a55777a0ac8

---
+++ comunica/feature/benchmark-watdiv-tpf/benchmarks-total/data.js
@@ -0,0 +1,42 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1717064425845,
+  "repoUrl": "https://github.com/comunica/comunica",
+  "entries": {
+    "Benchmarks total results": [
+      {
+        "commit": {
+          "author": {
+            "email": "ruben.taelman@ugent.be",
+            "name": "Ruben Taelman",
+            "username": "rubensworks"
+          },
+          "committer": {
+            "email": "ruben.taelman@ugent.be",
+            "name": "Ruben Taelman",
+            "username": "rubensworks"
+          },
+          "distinct": true,
+          "id": "542ae91daa2ccc176a4ff892989e7a55777a0ac8",
+          "message": "Add WatDiv TPF benchmark",
+          "timestamp": "2024-05-30T12:00:06+02:00",
+          "tree_id": "a596718cbab5c0464f6bc6b1fc5fd7594c39fa3b",
+          "url": "https://github.com/comunica/comunica/commit/542ae91daa2ccc176a4ff892989e7a55777a0ac8"
+        },
+        "date": 1717064425331,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "WatDiv-File",
+            "value": 7686,
+            "unit": "ms"
+          },
+          {
+            "name": "WatDiv-TPF",
+            "value": 34552,
+            "unit": "ms"
+          }
+        ]
+      }
+    ]
+  }
+}
