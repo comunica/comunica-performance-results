bcd9cd496f81ffca6eb110aa0547d11074b6cc46
github-action-benchmark <github@users.noreply.github.com>
2024-06-02 08:47:55 +0000

    add Benchmarks total results (customSmallerIsBetter) benchmark result for 00ab4771673db630084e9573f9d9d6247ce87c59

--- comunica-feature-hdt/master/benchmarks-total/data.js
+++ comunica-feature-hdt/master/benchmarks-total/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1717074890111,
+  "lastUpdate": 1717318075420,
   "repoUrl": "https://github.com/comunica/comunica-feature-hdt",
   "entries": {
     "Benchmarks total results": [
@@ -57,6 +57,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "WatDiv-HDT",
             "value": 6637,
+            "unit": "ms"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "29139614+renovate[bot]@users.noreply.github.com",
+            "name": "renovate[bot]",
+            "username": "renovate[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "00ab4771673db630084e9573f9d9d6247ce87c59",
+          "message": "Update Comunica monorepo packages to ^3.1.2",
+          "timestamp": "2024-05-31T05:48:24Z",
+          "tree_id": "3f6b4304e281cc13b34f977438a96470437d296a",
+          "url": "https://github.com/comunica/comunica-feature-hdt/commit/00ab4771673db630084e9573f9d9d6247ce87c59"
+        },
+        "date": 1717318075117,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "WatDiv-HDT",
+            "value": 5069,
             "unit": "ms"
           }
         ]
