8b935f5bb6dbdb6b0d7aa7a78daccd003307018b
github-action-benchmark <github@users.noreply.github.com>
2024-06-18 18:03:44 +0000

    add Benchmarks total results (customSmallerIsBetter) benchmark result for dc06027a3717d9d0273afea3776a80829f0551aa

--- comunica/feature/performance-bsbm/benchmarks-total/data.js
+++ comunica/feature/performance-bsbm/benchmarks-total/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1718732640079,
+  "lastUpdate": 1718733824834,
   "repoUrl": "https://github.com/comunica/comunica",
   "entries": {
     "Benchmarks total results": [
@@ -43,6 +43,50 @@ window.BENCHMARK_DATA = {
           {
             "name": "BSBM-TPF",
             "value": 2434,
+            "unit": "ms"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "ruben.taelman@ugent.be",
+            "name": "Ruben Taelman",
+            "username": "rubensworks"
+          },
+          "committer": {
+            "email": "ruben.taelman@ugent.be",
+            "name": "Ruben Taelman",
+            "username": "rubensworks"
+          },
+          "distinct": true,
+          "id": "dc06027a3717d9d0273afea3776a80829f0551aa",
+          "message": "Add BSBM benchmark",
+          "timestamp": "2024-06-18T19:49:15+02:00",
+          "tree_id": "6475d9737d13b418d5cc18ce6453e6e619bca8eb",
+          "url": "https://github.com/comunica/comunica/commit/dc06027a3717d9d0273afea3776a80829f0551aa"
+        },
+        "date": 1718733824246,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "WatDiv-File",
+            "value": 8084,
+            "unit": "ms"
+          },
+          {
+            "name": "WatDiv-TPF",
+            "value": 35142,
+            "unit": "ms"
+          },
+          {
+            "name": "BSBM-File",
+            "value": 1070,
+            "unit": "ms"
+          },
+          {
+            "name": "BSBM-TPF",
+            "value": 2516,
             "unit": "ms"
           }
         ]
