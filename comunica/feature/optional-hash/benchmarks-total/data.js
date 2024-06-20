58d865913194fa1265b81917e0cd52ea0c7a810c
github-action-benchmark <github@users.noreply.github.com>
2024-06-20 13:52:43 +0000

    add Benchmarks total results (customSmallerIsBetter) benchmark result for df40c20e001121cd0ae9a9adf67ed221dc2966ba

--- comunica/feature/optional-hash/benchmarks-total/data.js
+++ comunica/feature/optional-hash/benchmarks-total/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1718364260879,
+  "lastUpdate": 1718891563931,
   "repoUrl": "https://github.com/comunica/comunica",
   "entries": {
     "Benchmarks total results": [
@@ -33,6 +33,50 @@ window.BENCHMARK_DATA = {
           {
             "name": "WatDiv-TPF",
             "value": 36307,
+            "unit": "ms"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "ruben.taelman@ugent.be",
+            "name": "Ruben Taelman",
+            "username": "rubensworks"
+          },
+          "committer": {
+            "email": "ruben.taelman@ugent.be",
+            "name": "Ruben Taelman",
+            "username": "rubensworks"
+          },
+          "distinct": true,
+          "id": "df40c20e001121cd0ae9a9adf67ed221dc2966ba",
+          "message": "Make optional hash and bind join only work with common variables",
+          "timestamp": "2024-06-20T15:30:16+02:00",
+          "tree_id": "ed89e2f2455ea7bc3bf0ed3851ed620b5cb34057",
+          "url": "https://github.com/comunica/comunica/commit/df40c20e001121cd0ae9a9adf67ed221dc2966ba"
+        },
+        "date": 1718891563732,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "WatDiv-File",
+            "value": 8462,
+            "unit": "ms"
+          },
+          {
+            "name": "WatDiv-TPF",
+            "value": 36198,
+            "unit": "ms"
+          },
+          {
+            "name": "BSBM-File",
+            "value": 1113,
+            "unit": "ms"
+          },
+          {
+            "name": "BSBM-TPF",
+            "value": 2547,
             "unit": "ms"
           }
         ]
