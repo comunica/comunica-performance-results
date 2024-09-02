db3c31426d590f16e4e1520235ac8a4fcd114d95
github-action-benchmark <github@users.noreply.github.com>
2024-09-02 12:35:00 +0000

    add Benchmarks total results (customSmallerIsBetter) benchmark result for be5498a936db66e9d3ff87f89d8b064a01ca494c

---
+++ comunica/feature/test-nostack/benchmarks-total/data.js
@@ -0,0 +1,57 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1725280500743,
+  "repoUrl": "https://github.com/comunica/comunica",
+  "entries": {
+    "Benchmarks total results": [
+      {
+        "commit": {
+          "author": {
+            "email": "rubensworks@users.noreply.github.com",
+            "name": "Ruben Taelman",
+            "username": "rubensworks"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "be5498a936db66e9d3ff87f89d8b064a01ca494c",
+          "message": "Merge pull request #1417 from jitsedesmet/next/major\n\nMerge master into next/major",
+          "timestamp": "2024-08-23T08:29:22+02:00",
+          "tree_id": "31f990439e61eb5f29cd9e2b7fc2bd01c77b4cb3",
+          "url": "https://github.com/comunica/comunica/commit/be5498a936db66e9d3ff87f89d8b064a01ca494c"
+        },
+        "date": 1725280500209,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "WatDiv-File",
+            "value": 7644,
+            "unit": "ms"
+          },
+          {
+            "name": "WatDiv-TPF",
+            "value": 34546,
+            "unit": "ms"
+          },
+          {
+            "name": "BSBM-File",
+            "value": 1078,
+            "unit": "ms"
+          },
+          {
+            "name": "BSBM-TPF",
+            "value": 2362,
+            "unit": "ms"
+          },
+          {
+            "name": "Web",
+            "value": 232056,
+            "unit": "ms"
+          }
+        ]
+      }
+    ]
+  }
+}
