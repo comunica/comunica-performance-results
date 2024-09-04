1b1e3b69687ec939b6de04524a642dec6288b9cb
github-action-benchmark <github@users.noreply.github.com>
2024-09-04 13:58:21 +0000

    add Benchmarks detailed results (customSmallerIsBetter) benchmark result for 991793a03b53beddce541e375d4ca8b4171990a2

--- comunica-feature-link-traversal/master/benchmarks-detail/data.js
+++ comunica-feature-link-traversal/master/benchmarks-detail/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1725002230401,
+  "lastUpdate": 1725458301453,
   "repoUrl": "https://github.com/comunica/comunica-feature-link-traversal",
   "entries": {
     "Benchmarks detailed results": [
@@ -352,6 +352,78 @@ window.BENCHMARK_DATA = {
             "value": 9834,
             "unit": "ms",
             "extra": "Results: [0,10,0,0,0]; Error: [true,false,true,true,true]; HTTP Requests: [,352,,,]"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "ruben.taelman@ugent.be",
+            "name": "Ruben Taelman",
+            "username": "rubensworks"
+          },
+          "committer": {
+            "email": "ruben.taelman@ugent.be",
+            "name": "Ruben Taelman",
+            "username": "rubensworks"
+          },
+          "distinct": true,
+          "id": "991793a03b53beddce541e375d4ca8b4171990a2",
+          "message": "Update Dockerfiles to Node 22",
+          "timestamp": "2024-09-04T15:42:17+02:00",
+          "tree_id": "69fa5c10f044f63410de42e0eafb49ecfb072a60",
+          "url": "https://github.com/comunica/comunica-feature-link-traversal/commit/991793a03b53beddce541e375d4ca8b4171990a2"
+        },
+        "date": 1725458301133,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SolidBench - interactive-discover-1",
+            "value": 508,
+            "unit": "ms",
+            "extra": "Results: [0,4,1,14,8]; Error: [true,false,false,false,false]; HTTP Requests: [,49,42,29,44]"
+          },
+          {
+            "name": "SolidBench - interactive-discover-2",
+            "value": 569,
+            "unit": "ms",
+            "extra": "Results: [66,46,32,198,98]; Error: [false,false,false,false,false]; HTTP Requests: [50,49,42,29,44]"
+          },
+          {
+            "name": "SolidBench - interactive-discover-3",
+            "value": 1001,
+            "unit": "ms",
+            "extra": "Results: [71,67,55,203,142]; Error: [false,false,false,false,false]; HTTP Requests: [176,177,126,235,195]"
+          },
+          {
+            "name": "SolidBench - interactive-discover-4",
+            "value": 372,
+            "unit": "ms",
+            "extra": "Results: [5,3,3,3,7]; Error: [false,false,false,false,false]; HTTP Requests: [66,53,46,34,64]"
+          },
+          {
+            "name": "SolidBench - interactive-discover-5",
+            "value": 352,
+            "unit": "ms",
+            "extra": "Results: [15,3,3,4,20]; Error: [false,false,false,false,false]; HTTP Requests: [50,49,42,29,44]"
+          },
+          {
+            "name": "SolidBench - interactive-discover-6",
+            "value": 433,
+            "unit": "ms",
+            "extra": "Results: [0,1,1,0,27]; Error: [true,false,false,true,false]; HTTP Requests: [,49,42,,44]"
+          },
+          {
+            "name": "SolidBench - interactive-discover-7",
+            "value": 401,
+            "unit": "ms",
+            "extra": "Results: [0,1,1,0,1]; Error: [true,false,false,true,false]; HTTP Requests: [,49,42,,44]"
+          },
+          {
+            "name": "SolidBench - interactive-discover-8",
+            "value": 7074,
+            "unit": "ms",
+            "extra": "Results: [0,10,0,0,10]; Error: [true,false,true,true,false]; HTTP Requests: [,282,,,336]"
           }
         ]
       }
