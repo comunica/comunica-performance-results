08412ce253a26ebceb198300f381723c467bf149
github-action-benchmark <github@users.noreply.github.com>
2024-09-04 10:03:29 +0000

    add Benchmarks total results (customSmallerIsBetter) benchmark result for db38bf3df3bd1765e51bcb832dcb936706451606

--- comunica/feature/test-nostack/benchmarks-total/data.js
+++ comunica/feature/test-nostack/benchmarks-total/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1725280500743,
+  "lastUpdate": 1725444209931,
   "repoUrl": "https://github.com/comunica/comunica",
   "entries": {
     "Benchmarks total results": [
@@ -48,6 +48,55 @@ window.BENCHMARK_DATA = {
           {
             "name": "Web",
             "value": 232056,
+            "unit": "ms"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "ruben.taelman@ugent.be",
+            "name": "Ruben Taelman",
+            "username": "rubensworks"
+          },
+          "committer": {
+            "email": "ruben.taelman@ugent.be",
+            "name": "Ruben Taelman",
+            "username": "rubensworks"
+          },
+          "distinct": true,
+          "id": "db38bf3df3bd1765e51bcb832dcb936706451606",
+          "message": "Refactor Actor.test to not rely on Promise rejections\n\nPromise rejections rely on the creation of Error objects,\nwhich are expensive to create in large numbers due to stack trace\nbuilding.\nSince Comunica tends to invoke a huge number of test methods for an\naverage query exection, this can cause a large performance overhead.\n\nThis commit refactors test methods to asynchronously return a\nTestResult, which is a holder object that either contains a passed value\nor a failure message.",
+          "timestamp": "2024-09-04T11:36:16+02:00",
+          "tree_id": "b6f1b7eaa7bd16db0c1b4774ce978fcdd757c0b8",
+          "url": "https://github.com/comunica/comunica/commit/db38bf3df3bd1765e51bcb832dcb936706451606"
+        },
+        "date": 1725444209584,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "WatDiv-File",
+            "value": 6991,
+            "unit": "ms"
+          },
+          {
+            "name": "WatDiv-TPF",
+            "value": 31121,
+            "unit": "ms"
+          },
+          {
+            "name": "BSBM-File",
+            "value": 635,
+            "unit": "ms"
+          },
+          {
+            "name": "BSBM-TPF",
+            "value": 1940,
+            "unit": "ms"
+          },
+          {
+            "name": "Web",
+            "value": 131593,
             "unit": "ms"
           }
         ]
