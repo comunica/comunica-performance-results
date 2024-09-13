5d05e265c4eeb6ea9276910f80964f78dabb6a92
github-action-benchmark <github@users.noreply.github.com>
2024-09-13 10:18:49 +0000

    add Benchmarks total results (customSmallerIsBetter) benchmark result for 5ddb64bb1df4b1179529f3e2ea0c05fb321dfce9

--- comunica/next/major/benchmarks-total/data.js
+++ comunica/next/major/benchmarks-total/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1726144883666,
+  "lastUpdate": 1726222729155,
   "repoUrl": "https://github.com/comunica/comunica",
   "entries": {
     "Benchmarks total results": [
@@ -605,6 +605,55 @@ window.BENCHMARK_DATA = {
           {
             "name": "Web",
             "value": 132073,
+            "unit": "ms"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "ruben.taelman@ugent.be",
+            "name": "Ruben Taelman",
+            "username": "rubensworks"
+          },
+          "committer": {
+            "email": "ruben.taelman@ugent.be",
+            "name": "Ruben Taelman",
+            "username": "rubensworks"
+          },
+          "distinct": false,
+          "id": "5ddb64bb1df4b1179529f3e2ea0c05fb321dfce9",
+          "message": "Only consider overlapping vars when testing undef in join actors\n\nThis makes it so that actors that can not handle undefs to still work\nover bindings streams that have undefs, but only for variables that are\nirrelevant to the joining.\n\nThis allows for more efficient join actors to be selectable for certain\nqueries, such as other/hubl-optional.sparql.",
+          "timestamp": "2024-09-13T11:53:08+02:00",
+          "tree_id": "b1bb64326c78e9a4dd43d50aee72b9f1f94d057e",
+          "url": "https://github.com/comunica/comunica/commit/5ddb64bb1df4b1179529f3e2ea0c05fb321dfce9"
+        },
+        "date": 1726222728964,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "WatDiv-File",
+            "value": 5565,
+            "unit": "ms"
+          },
+          {
+            "name": "WatDiv-TPF",
+            "value": 23230,
+            "unit": "ms"
+          },
+          {
+            "name": "BSBM-File",
+            "value": 595,
+            "unit": "ms"
+          },
+          {
+            "name": "BSBM-TPF",
+            "value": 1615,
+            "unit": "ms"
+          },
+          {
+            "name": "Web",
+            "value": 131534,
             "unit": "ms"
           }
         ]
