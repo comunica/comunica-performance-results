7c49e9be9b62ca3085d5497737d3479ba9ed4b6e
github-action-benchmark <github@users.noreply.github.com>
2024-09-19 16:01:10 +0000

    add Benchmarks total results (customSmallerIsBetter) benchmark result for 7ed87f093f363b78c2ab743cb4e2d6abf184a401

--- comunica/feature/forward-test-run-tmp/benchmarks-total/data.js
+++ comunica/feature/forward-test-run-tmp/benchmarks-total/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1726756981065,
+  "lastUpdate": 1726761670818,
   "repoUrl": "https://github.com/comunica/comunica",
   "entries": {
     "Benchmarks total results": [
@@ -48,6 +48,55 @@ window.BENCHMARK_DATA = {
           {
             "name": "Web",
             "value": 133069,
+            "unit": "ms"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "ruben.taelman@ugent.be",
+            "name": "Ruben Taelman",
+            "username": "rubensworks"
+          },
+          "committer": {
+            "email": "ruben.taelman@ugent.be",
+            "name": "Ruben Taelman",
+            "username": "rubensworks"
+          },
+          "distinct": true,
+          "id": "7ed87f093f363b78c2ab743cb4e2d6abf184a401",
+          "message": "Forward side data from test to run in actors\n\nThis allows expensive logic from test methods to be reused as input to\nrun methods.\n\nCloses #1006",
+          "timestamp": "2024-09-19T17:06:02+02:00",
+          "tree_id": "c996e49ef9c38239d745aaf281d6358f4683a774",
+          "url": "https://github.com/comunica/comunica/commit/7ed87f093f363b78c2ab743cb4e2d6abf184a401"
+        },
+        "date": 1726761670488,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "WatDiv-File",
+            "value": 6715,
+            "unit": "ms"
+          },
+          {
+            "name": "WatDiv-TPF",
+            "value": 22777,
+            "unit": "ms"
+          },
+          {
+            "name": "BSBM-File",
+            "value": 600,
+            "unit": "ms"
+          },
+          {
+            "name": "BSBM-TPF",
+            "value": 1540,
+            "unit": "ms"
+          },
+          {
+            "name": "Web",
+            "value": 157751,
             "unit": "ms"
           }
         ]
