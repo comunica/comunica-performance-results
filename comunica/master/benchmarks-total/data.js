d3e13bf15f74623b6d5b0e17851c8da2cd568898
github-action-benchmark <github@users.noreply.github.com>
2024-09-20 09:32:35 +0000

    add Benchmarks total results (customSmallerIsBetter) benchmark result for a83d31964a63925d7cf3b60b6bd32b7c825fab8d

--- comunica/master/benchmarks-total/data.js
+++ comunica/master/benchmarks-total/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1726814849163,
+  "lastUpdate": 1726824755091,
   "repoUrl": "https://github.com/comunica/comunica",
   "entries": {
     "Benchmarks total results": [
@@ -2347,6 +2347,55 @@ window.BENCHMARK_DATA = {
           {
             "name": "Web",
             "value": 156142,
+            "unit": "ms"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "bryanelliott.tam@ugent.be",
+            "name": "Bryan-Elliott Tam",
+            "username": "constraintAutomaton"
+          },
+          "committer": {
+            "email": "rubensworks@users.noreply.github.com",
+            "name": "Ruben Taelman",
+            "username": "rubensworks"
+          },
+          "distinct": true,
+          "id": "a83d31964a63925d7cf3b60b6bd32b7c825fab8d",
+          "message": "Fix the typo where the actor refers to QuerySourceQpf instead of QuerySourceRdfJs in the error thrown",
+          "timestamp": "2024-09-20T11:07:47+02:00",
+          "tree_id": "dd895e9a3940f973ad106e8b3a4d1da9c5127680",
+          "url": "https://github.com/comunica/comunica/commit/a83d31964a63925d7cf3b60b6bd32b7c825fab8d"
+        },
+        "date": 1726824754531,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "WatDiv-File",
+            "value": 6018,
+            "unit": "ms"
+          },
+          {
+            "name": "WatDiv-TPF",
+            "value": 26744,
+            "unit": "ms"
+          },
+          {
+            "name": "BSBM-File",
+            "value": 939,
+            "unit": "ms"
+          },
+          {
+            "name": "BSBM-TPF",
+            "value": 1985,
+            "unit": "ms"
+          },
+          {
+            "name": "Web",
+            "value": 133183,
             "unit": "ms"
           }
         ]
