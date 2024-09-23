8b6fa9756b682b5c6055843911d14975aa638762
github-action-benchmark <github@users.noreply.github.com>
2024-09-23 12:09:26 +0000

    add Benchmarks total results (customSmallerIsBetter) benchmark result for c1474e4167f1978e144bee9b7663fcb2bafc21bf

--- comunica/feature/hash-non-colliding/benchmarks-total/data.js
+++ comunica/feature/hash-non-colliding/benchmarks-total/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1726840100254,
+  "lastUpdate": 1727093366112,
   "repoUrl": "https://github.com/comunica/comunica",
   "entries": {
     "Benchmarks total results": [
@@ -48,6 +48,55 @@ window.BENCHMARK_DATA = {
           {
             "name": "Web",
             "value": 130694,
+            "unit": "ms"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "ruben.taelman@ugent.be",
+            "name": "Ruben Taelman",
+            "username": "rubensworks"
+          },
+          "committer": {
+            "email": "ruben.taelman@ugent.be",
+            "name": "Ruben Taelman",
+            "username": "rubensworks"
+          },
+          "distinct": true,
+          "id": "c1474e4167f1978e144bee9b7663fcb2bafc21bf",
+          "message": "Return 32-bit numbers in hash bus based on specific variablesg",
+          "timestamp": "2024-09-23T13:28:55+02:00",
+          "tree_id": "df2ca9375a8c4f062e6d9b1afadc8de459b05041",
+          "url": "https://github.com/comunica/comunica/commit/c1474e4167f1978e144bee9b7663fcb2bafc21bf"
+        },
+        "date": 1727093365419,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "WatDiv-File",
+            "value": 5849,
+            "unit": "ms"
+          },
+          {
+            "name": "WatDiv-TPF",
+            "value": 23758,
+            "unit": "ms"
+          },
+          {
+            "name": "BSBM-File",
+            "value": 679,
+            "unit": "ms"
+          },
+          {
+            "name": "BSBM-TPF",
+            "value": 1543,
+            "unit": "ms"
+          },
+          {
+            "name": "Web",
+            "value": 163232,
             "unit": "ms"
           }
         ]
