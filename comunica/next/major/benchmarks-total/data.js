79af782c5ddcd82716163b3abb638561a461e0b3
github-action-benchmark <github@users.noreply.github.com>
2024-09-23 15:19:50 +0000

    add Benchmarks total results (customSmallerIsBetter) benchmark result for 003d587396edffc922a7ae7dcec209c6b61929c9

--- comunica/next/major/benchmarks-total/data.js
+++ comunica/next/major/benchmarks-total/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1726836405576,
+  "lastUpdate": 1727104790928,
   "repoUrl": "https://github.com/comunica/comunica",
   "entries": {
     "Benchmarks total results": [
@@ -850,6 +850,55 @@ window.BENCHMARK_DATA = {
           {
             "name": "Web",
             "value": 130771,
+            "unit": "ms"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "ruben.taelman@ugent.be",
+            "name": "Ruben Taelman",
+            "username": "rubensworks"
+          },
+          "committer": {
+            "email": "ruben.taelman@ugent.be",
+            "name": "Ruben Taelman",
+            "username": "rubensworks"
+          },
+          "distinct": false,
+          "id": "003d587396edffc922a7ae7dcec209c6b61929c9",
+          "message": "Move ActorRdfJoin.hashNonClashing to bindingsToCompactString",
+          "timestamp": "2024-09-23T15:58:56+02:00",
+          "tree_id": "f67ec0798c136a3e269fd70de67af84219df2590",
+          "url": "https://github.com/comunica/comunica/commit/003d587396edffc922a7ae7dcec209c6b61929c9"
+        },
+        "date": 1727104790698,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "WatDiv-File",
+            "value": 5872,
+            "unit": "ms"
+          },
+          {
+            "name": "WatDiv-TPF",
+            "value": 24232,
+            "unit": "ms"
+          },
+          {
+            "name": "BSBM-File",
+            "value": 513,
+            "unit": "ms"
+          },
+          {
+            "name": "BSBM-TPF",
+            "value": 1524,
+            "unit": "ms"
+          },
+          {
+            "name": "Web",
+            "value": 132815,
             "unit": "ms"
           }
         ]
