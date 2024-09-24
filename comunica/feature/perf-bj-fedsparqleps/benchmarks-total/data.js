228a013267fd70e04be4b0a6c7a46280a69c495e
github-action-benchmark <github@users.noreply.github.com>
2024-09-24 15:48:44 +0000

    add Benchmarks total results (customSmallerIsBetter) benchmark result for a83d31964a63925d7cf3b60b6bd32b7c825fab8d

---
+++ comunica/feature/perf-bj-fedsparqleps/benchmarks-total/data.js
@@ -0,0 +1,57 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1727192924327,
+  "repoUrl": "https://github.com/comunica/comunica",
+  "entries": {
+    "Benchmarks total results": [
+      {
+        "commit": {
+          "author": {
+            "email": "bryanelliott.tam@ugent.be",
+            "name": "Bryan-Elliott Tam",
+            "username": "constraintAutomaton"
+          },
+          "committer": {
+            "email": "rubensworks@users.noreply.github.com",
+            "name": "Ruben Taelman",
+            "username": "rubensworks"
+          },
+          "distinct": true,
+          "id": "a83d31964a63925d7cf3b60b6bd32b7c825fab8d",
+          "message": "Fix the typo where the actor refers to QuerySourceQpf instead of QuerySourceRdfJs in the error thrown",
+          "timestamp": "2024-09-20T11:07:47+02:00",
+          "tree_id": "dd895e9a3940f973ad106e8b3a4d1da9c5127680",
+          "url": "https://github.com/comunica/comunica/commit/a83d31964a63925d7cf3b60b6bd32b7c825fab8d"
+        },
+        "date": 1727192923751,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "WatDiv-File",
+            "value": 6325,
+            "unit": "ms"
+          },
+          {
+            "name": "WatDiv-TPF",
+            "value": 27308,
+            "unit": "ms"
+          },
+          {
+            "name": "BSBM-File",
+            "value": 849,
+            "unit": "ms"
+          },
+          {
+            "name": "BSBM-TPF",
+            "value": 2007,
+            "unit": "ms"
+          },
+          {
+            "name": "Web",
+            "value": 203480,
+            "unit": "ms"
+          }
+        ]
+      }
+    ]
+  }
+}
