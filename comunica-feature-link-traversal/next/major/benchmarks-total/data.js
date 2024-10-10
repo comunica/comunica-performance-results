ad7d550dd7117997d588a2f3cdc78adca8196dd8
github-action-benchmark <github@users.noreply.github.com>
2024-10-10 12:33:19 +0000

    add Benchmarks total results (customSmallerIsBetter) benchmark result for c88324b210d656899e9dcac186cadf06d68512ed

--- comunica-feature-link-traversal/next/major/benchmarks-total/data.js
+++ comunica-feature-link-traversal/next/major/benchmarks-total/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1728560359416,
+  "lastUpdate": 1728563599217,
   "repoUrl": "https://github.com/comunica/comunica-feature-link-traversal",
   "entries": {
     "Benchmarks total results": [
@@ -28,6 +28,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "SolidBench",
             "value": 12237,
+            "unit": "ms"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "ruben.taelman@ugent.be",
+            "name": "Ruben Taelman",
+            "username": "rubensworks"
+          },
+          "committer": {
+            "email": "ruben.taelman@ugent.be",
+            "name": "Ruben Taelman",
+            "username": "rubensworks"
+          },
+          "distinct": true,
+          "id": "c88324b210d656899e9dcac186cadf06d68512ed",
+          "message": "Update to Comunica v4 prerelease",
+          "timestamp": "2024-10-10T14:17:03+02:00",
+          "tree_id": "0cc598e4d79d5fad3100453d8afd953cc9aae6ad",
+          "url": "https://github.com/comunica/comunica-feature-link-traversal/commit/c88324b210d656899e9dcac186cadf06d68512ed"
+        },
+        "date": 1728563598929,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SolidBench",
+            "value": 12452,
             "unit": "ms"
           }
         ]
