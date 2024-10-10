8961e9a0b2c00d5c12ec36b63a7263b73345e038
github-action-benchmark <github@users.noreply.github.com>
2024-10-10 11:39:19 +0000

    add Benchmarks total results (customSmallerIsBetter) benchmark result for 991793a03b53beddce541e375d4ca8b4171990a2

---
+++ comunica-feature-link-traversal/next/major/benchmarks-total/data.js
@@ -0,0 +1,37 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1728560359416,
+  "repoUrl": "https://github.com/comunica/comunica-feature-link-traversal",
+  "entries": {
+    "Benchmarks total results": [
+      {
+        "commit": {
+          "author": {
+            "email": "ruben.taelman@ugent.be",
+            "name": "Ruben Taelman",
+            "username": "rubensworks"
+          },
+          "committer": {
+            "email": "ruben.taelman@ugent.be",
+            "name": "Ruben Taelman",
+            "username": "rubensworks"
+          },
+          "distinct": true,
+          "id": "991793a03b53beddce541e375d4ca8b4171990a2",
+          "message": "Update Dockerfiles to Node 22",
+          "timestamp": "2024-09-04T15:42:17+02:00",
+          "tree_id": "69fa5c10f044f63410de42e0eafb49ecfb072a60",
+          "url": "https://github.com/comunica/comunica-feature-link-traversal/commit/991793a03b53beddce541e375d4ca8b4171990a2"
+        },
+        "date": 1728560359151,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SolidBench",
+            "value": 12237,
+            "unit": "ms"
+          }
+        ]
+      }
+    ]
+  }
+}
