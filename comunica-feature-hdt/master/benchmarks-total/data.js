6732a0b1742d43028ef038fbbc0472ac2e412118
github-action-benchmark <github@users.noreply.github.com>
2024-10-15 09:03:56 +0000

    add Benchmarks total results (customSmallerIsBetter) benchmark result for cfaeb9efe548fd8452665c626e509f5e820f5389

--- comunica-feature-hdt/master/benchmarks-total/data.js
+++ comunica-feature-hdt/master/benchmarks-total/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1725461193207,
+  "lastUpdate": 1728983036196,
   "repoUrl": "https://github.com/comunica/comunica-feature-hdt",
   "entries": {
     "Benchmarks total results": [
@@ -557,6 +557,40 @@ window.BENCHMARK_DATA = {
           {
             "name": "BSBM-HDT",
             "value": 770,
+            "unit": "ms"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "ruben.taelman@ugent.be",
+            "name": "Ruben Taelman",
+            "username": "rubensworks"
+          },
+          "committer": {
+            "email": "ruben.taelman@ugent.be",
+            "name": "Ruben Taelman",
+            "username": "rubensworks"
+          },
+          "distinct": true,
+          "id": "cfaeb9efe548fd8452665c626e509f5e820f5389",
+          "message": "Bump to release version v4.0.1",
+          "timestamp": "2024-10-15T10:54:42+02:00",
+          "tree_id": "1aa03a8b704f6c5e9851f7026cf14e7ff4829455",
+          "url": "https://github.com/comunica/comunica-feature-hdt/commit/cfaeb9efe548fd8452665c626e509f5e820f5389"
+        },
+        "date": 1728983035916,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "WatDiv-HDT",
+            "value": 2996,
+            "unit": "ms"
+          },
+          {
+            "name": "BSBM-HDT",
+            "value": 424,
             "unit": "ms"
           }
         ]
