cd2ac3366dc6157048c76aeb2be62b176468cf89
github-action-benchmark <github@users.noreply.github.com>
2024-10-15 09:09:18 +0000

    add Benchmarks total results (customSmallerIsBetter) benchmark result for 658ec9ab431031a56874c4262532c5dc23a8d327

--- comunica-feature-link-traversal/master/benchmarks-total/data.js
+++ comunica-feature-link-traversal/master/benchmarks-total/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1725458302231,
+  "lastUpdate": 1728983358521,
   "repoUrl": "https://github.com/comunica/comunica-feature-link-traversal",
   "entries": {
     "Benchmarks total results": [
@@ -173,6 +173,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "SolidBench",
             "value": 10710,
+            "unit": "ms"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "ruben.taelman@ugent.be",
+            "name": "Ruben Taelman",
+            "username": "rubensworks"
+          },
+          "committer": {
+            "email": "ruben.taelman@ugent.be",
+            "name": "Ruben Taelman",
+            "username": "rubensworks"
+          },
+          "distinct": true,
+          "id": "658ec9ab431031a56874c4262532c5dc23a8d327",
+          "message": "Bump to release version v0.6.0",
+          "timestamp": "2024-10-15T10:46:33+02:00",
+          "tree_id": "34c3ba94d1e856795a854dead6a32a036b65bdea",
+          "url": "https://github.com/comunica/comunica-feature-link-traversal/commit/658ec9ab431031a56874c4262532c5dc23a8d327"
+        },
+        "date": 1728983358118,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SolidBench",
+            "value": 3151,
             "unit": "ms"
           }
         ]
