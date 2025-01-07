932dd97f57173d5899a9e110739f184d0d1325ac
github-action-benchmark <github@users.noreply.github.com>
2025-01-07 14:35:53 +0000

    add Benchmarks total results (customSmallerIsBetter) benchmark result for 22d63135de676e83745848ac78349a3647594fcd

--- comunica/master/benchmarks-total/data.js
+++ comunica/master/benchmarks-total/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1736258627593,
+  "lastUpdate": 1736260553665,
   "repoUrl": "https://github.com/comunica/comunica",
   "entries": {
     "Benchmarks total results": [
@@ -3376,6 +3376,55 @@ window.BENCHMARK_DATA = {
           {
             "name": "Web",
             "value": 159183,
+            "unit": "ms"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "29139614+renovate[bot]@users.noreply.github.com",
+            "name": "renovate[bot]",
+            "username": "renovate[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "22d63135de676e83745848ac78349a3647594fcd",
+          "message": "Update dependency htmlparser2 to v10 (#1472)\n\nCo-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>",
+          "timestamp": "2025-01-07T15:22:10+01:00",
+          "tree_id": "5221c02175aefcb23d6c094093112bb856266aa3",
+          "url": "https://github.com/comunica/comunica/commit/22d63135de676e83745848ac78349a3647594fcd"
+        },
+        "date": 1736260553292,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "WatDiv-File",
+            "value": 5430,
+            "unit": "ms"
+          },
+          {
+            "name": "WatDiv-TPF",
+            "value": 140,
+            "unit": "ms"
+          },
+          {
+            "name": "BSBM-File",
+            "value": 409,
+            "unit": "ms"
+          },
+          {
+            "name": "BSBM-TPF",
+            "value": 93,
+            "unit": "ms"
+          },
+          {
+            "name": "Web",
+            "value": 137614,
             "unit": "ms"
           }
         ]
