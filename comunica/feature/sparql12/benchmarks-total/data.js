5d1bfd688423f87673721d29d41c1c85662e15ba
github-action-benchmark <github@users.noreply.github.com>
2025-01-09 13:18:08 +0000

    add Benchmarks total results (customSmallerIsBetter) benchmark result for 830fbee0045f15b7ec345ea4d64a5a152b7b5f12

--- comunica/feature/sparql12/benchmarks-total/data.js
+++ comunica/feature/sparql12/benchmarks-total/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1736345395100,
+  "lastUpdate": 1736428688760,
   "repoUrl": "https://github.com/comunica/comunica",
   "entries": {
     "Benchmarks total results": [
@@ -48,6 +48,55 @@ window.BENCHMARK_DATA = {
           {
             "name": "Web",
             "value": 148661,
+            "unit": "ms"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "ruben.taelman@ugent.be",
+            "name": "Ruben Taelman",
+            "username": "rubensworks"
+          },
+          "committer": {
+            "email": "ruben.taelman@ugent.be",
+            "name": "Ruben Taelman",
+            "username": "rubensworks"
+          },
+          "distinct": true,
+          "id": "830fbee0045f15b7ec345ea4d64a5a152b7b5f12",
+          "message": "Update to rdf-data-factory@2 and @rdfjs/types@2\n\nThese add support for base directions in literals.",
+          "timestamp": "2025-01-09T14:02:31+01:00",
+          "tree_id": "7638cba9b27244663241aa69eb57e3f6b739cff4",
+          "url": "https://github.com/comunica/comunica/commit/830fbee0045f15b7ec345ea4d64a5a152b7b5f12"
+        },
+        "date": 1736428688335,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "WatDiv-File",
+            "value": 5342,
+            "unit": "ms"
+          },
+          {
+            "name": "WatDiv-TPF",
+            "value": 121,
+            "unit": "ms"
+          },
+          {
+            "name": "BSBM-File",
+            "value": 449,
+            "unit": "ms"
+          },
+          {
+            "name": "BSBM-TPF",
+            "value": 86,
+            "unit": "ms"
+          },
+          {
+            "name": "Web",
+            "value": 146609,
             "unit": "ms"
           }
         ]
