b132253020ea3503c3483bfb536e2a6734c39292
github-action-benchmark <github@users.noreply.github.com>
2025-02-24 07:58:09 +0000

    add Benchmarks total results (customSmallerIsBetter) benchmark result for 3bad47ca1bbe2581c92dd54d049e0caba537db94

--- comunica/feature/test-docker/benchmarks-total/data.js
+++ comunica/feature/test-docker/benchmarks-total/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1740065752309,
+  "lastUpdate": 1740383889689,
   "repoUrl": "https://github.com/comunica/comunica",
   "entries": {
     "Benchmarks total results": [
@@ -48,6 +48,55 @@ window.BENCHMARK_DATA = {
           {
             "name": "Web",
             "value": 150308,
+            "unit": "ms"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "ruben.taelman@ugent.be",
+            "name": "Ruben Taelman",
+            "username": "rubensworks"
+          },
+          "committer": {
+            "email": "ruben.taelman@ugent.be",
+            "name": "Ruben Taelman",
+            "username": "rubensworks"
+          },
+          "distinct": true,
+          "id": "3bad47ca1bbe2581c92dd54d049e0caba537db94",
+          "message": "Add Docker test to CI",
+          "timestamp": "2025-02-24T08:19:04+01:00",
+          "tree_id": "268a9691bed81a2f2de2b34be299d9e26d3068bc",
+          "url": "https://github.com/comunica/comunica/commit/3bad47ca1bbe2581c92dd54d049e0caba537db94"
+        },
+        "date": 1740383889311,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "WatDiv-File",
+            "value": 4791,
+            "unit": "ms"
+          },
+          {
+            "name": "WatDiv-TPF",
+            "value": 24550,
+            "unit": "ms"
+          },
+          {
+            "name": "BSBM-File",
+            "value": 342,
+            "unit": "ms"
+          },
+          {
+            "name": "BSBM-TPF",
+            "value": 1455,
+            "unit": "ms"
+          },
+          {
+            "name": "Web",
+            "value": 184575,
             "unit": "ms"
           }
         ]
