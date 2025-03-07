ce72f5efdee16cff82075c2effdb70a9be30afc0
github-action-benchmark <github@users.noreply.github.com>
2025-03-07 14:42:22 +0000

    add Benchmarks total results (customSmallerIsBetter) benchmark result for 6d4bdd813284676ae4a089739b9205b8fac08deb

--- comunica/feature/perf-bsbm-8/benchmarks-total/data.js
+++ comunica/feature/perf-bsbm-8/benchmarks-total/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1741357179621,
+  "lastUpdate": 1741358542765,
   "repoUrl": "https://github.com/comunica/comunica",
   "entries": {
     "Benchmarks total results": [
@@ -48,6 +48,55 @@ window.BENCHMARK_DATA = {
           {
             "name": "Web",
             "value": 162581,
+            "unit": "ms"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "16085353+surilindur@users.noreply.github.com",
+            "name": "surilindur",
+            "username": "surilindur"
+          },
+          "committer": {
+            "email": "ruben.taelman@ugent.be",
+            "name": "Ruben Taelman",
+            "username": "rubensworks"
+          },
+          "distinct": true,
+          "id": "6d4bdd813284676ae4a089739b9205b8fac08deb",
+          "message": "Add HTTP rate limit actor",
+          "timestamp": "2025-03-07T15:21:17+01:00",
+          "tree_id": "0a271426aaa33c45b9b1c995aaa5c5c62251fed4",
+          "url": "https://github.com/comunica/comunica/commit/6d4bdd813284676ae4a089739b9205b8fac08deb"
+        },
+        "date": 1741358541795,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "WatDiv-File",
+            "value": 4982,
+            "unit": "ms"
+          },
+          {
+            "name": "WatDiv-TPF",
+            "value": 23791,
+            "unit": "ms"
+          },
+          {
+            "name": "BSBM-File",
+            "value": 354,
+            "unit": "ms"
+          },
+          {
+            "name": "BSBM-TPF",
+            "value": 1413,
+            "unit": "ms"
+          },
+          {
+            "name": "Web",
+            "value": 232889,
             "unit": "ms"
           }
         ]
