21c47f3493673ab9d266624388b81cddc087f55f
github-action-benchmark <github@users.noreply.github.com>
2025-03-07 14:19:39 +0000

    add Benchmarks total results (customSmallerIsBetter) benchmark result for fb5962c885938f47165f40c31187441bccfe2d56

---
+++ comunica/feature/perf-bsbm-8/benchmarks-total/data.js
@@ -0,0 +1,57 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1741357179621,
+  "repoUrl": "https://github.com/comunica/comunica",
+  "entries": {
+    "Benchmarks total results": [
+      {
+        "commit": {
+          "author": {
+            "email": "16085353+surilindur@users.noreply.github.com",
+            "name": "surilindur",
+            "username": "surilindur"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "fb5962c885938f47165f40c31187441bccfe2d56",
+          "message": "Add HTTP rate limit actor",
+          "timestamp": "2025-03-04T12:49:24+01:00",
+          "tree_id": "0a271426aaa33c45b9b1c995aaa5c5c62251fed4",
+          "url": "https://github.com/comunica/comunica/commit/fb5962c885938f47165f40c31187441bccfe2d56"
+        },
+        "date": 1741357178800,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "WatDiv-File",
+            "value": 4922,
+            "unit": "ms"
+          },
+          {
+            "name": "WatDiv-TPF",
+            "value": 24987,
+            "unit": "ms"
+          },
+          {
+            "name": "BSBM-File",
+            "value": 361,
+            "unit": "ms"
+          },
+          {
+            "name": "BSBM-TPF",
+            "value": 1374,
+            "unit": "ms"
+          },
+          {
+            "name": "Web",
+            "value": 162581,
+            "unit": "ms"
+          }
+        ]
+      }
+    ]
+  }
+}
