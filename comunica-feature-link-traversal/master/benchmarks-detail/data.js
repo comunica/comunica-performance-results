4a8e29d13262c1af4e2c6a1d7273218308f3360b
github-action-benchmark <github@users.noreply.github.com>
2025-03-20 11:03:05 +0000

    add Benchmarks detailed results (customSmallerIsBetter) benchmark result for be8d8c362a75d1e88cbac08f36bcb512a8a96255

--- comunica-feature-link-traversal/master/benchmarks-detail/data.js
+++ comunica-feature-link-traversal/master/benchmarks-detail/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1736348220903,
+  "lastUpdate": 1742468585373,
   "repoUrl": "https://github.com/comunica/comunica-feature-link-traversal",
   "entries": {
     "Benchmarks detailed results": [
@@ -1072,6 +1072,78 @@ window.BENCHMARK_DATA = {
             "value": 8588,
             "unit": "ms",
             "extra": "Results: [0,10,0,10,0]; Error: [true,false,true,false,true]; HTTP Requests: [,352,,384,]"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "61841193+RubenEschauzier@users.noreply.github.com",
+            "name": "RubenEschauzier",
+            "username": "RubenEschauzier"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "be8d8c362a75d1e88cbac08f36bcb512a8a96255",
+          "message": "Add discovery tracking of extracted seed URIs from query (#169)",
+          "timestamp": "2025-03-20T11:43:31+01:00",
+          "tree_id": "699aa0c34ceb139de010405c94c4239507bed2e8",
+          "url": "https://github.com/comunica/comunica-feature-link-traversal/commit/be8d8c362a75d1e88cbac08f36bcb512a8a96255"
+        },
+        "date": 1742468584764,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SolidBench - interactive-discover-1",
+            "value": 375,
+            "unit": "ms",
+            "extra": "Results: [6,4,1,14,8]; Error: [false,false,false,false,false]; HTTP Requests: [50,49,42,29,44]"
+          },
+          {
+            "name": "SolidBench - interactive-discover-2",
+            "value": 353,
+            "unit": "ms",
+            "extra": "Results: [66,46,32,198,98]; Error: [false,false,false,false,false]; HTTP Requests: [50,49,42,29,44]"
+          },
+          {
+            "name": "SolidBench - interactive-discover-3",
+            "value": 960,
+            "unit": "ms",
+            "extra": "Results: [71,67,55,203,142]; Error: [false,false,false,false,false]; HTTP Requests: [176,177,126,235,195]"
+          },
+          {
+            "name": "SolidBench - interactive-discover-4",
+            "value": 317,
+            "unit": "ms",
+            "extra": "Results: [5,3,3,3,7]; Error: [false,false,false,false,false]; HTTP Requests: [66,53,46,34,64]"
+          },
+          {
+            "name": "SolidBench - interactive-discover-5",
+            "value": 205,
+            "unit": "ms",
+            "extra": "Results: [15,3,3,4,20]; Error: [false,false,false,false,false]; HTTP Requests: [50,49,42,29,44]"
+          },
+          {
+            "name": "SolidBench - interactive-discover-6",
+            "value": 393,
+            "unit": "ms",
+            "extra": "Results: [0,1,1,0,27]; Error: [true,false,false,true,false]; HTTP Requests: [,49,42,,44]"
+          },
+          {
+            "name": "SolidBench - interactive-discover-7",
+            "value": 410,
+            "unit": "ms",
+            "extra": "Results: [0,1,1,0,1]; Error: [true,false,false,true,false]; HTTP Requests: [,49,42,,44]"
+          },
+          {
+            "name": "SolidBench - interactive-discover-8",
+            "value": 7359,
+            "unit": "ms",
+            "extra": "Results: [10,0,0,0,10]; Error: [false,true,true,true,false]; HTTP Requests: [473,,,,319]"
           }
         ]
       }
