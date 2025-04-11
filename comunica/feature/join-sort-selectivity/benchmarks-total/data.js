67950008cae1084111da8fcc8f182f4056b6b6f0
github-action-benchmark <github@users.noreply.github.com>
2025-04-11 14:12:27 +0000

    add Benchmarks total results (customSmallerIsBetter) benchmark result for c5529ac2e01addeb1b1a5afbdbf9e29983ce1a93

---
+++ comunica/feature/join-sort-selectivity/benchmarks-total/data.js
@@ -0,0 +1,57 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1744380747471,
+  "repoUrl": "https://github.com/comunica/comunica",
+  "entries": {
+    "Benchmarks total results": [
+      {
+        "commit": {
+          "author": {
+            "email": "ruben.taelman@ugent.be",
+            "name": "Ruben Taelman",
+            "username": "rubensworks"
+          },
+          "committer": {
+            "email": "ruben.taelman@ugent.be",
+            "name": "Ruben Taelman",
+            "username": "rubensworks"
+          },
+          "distinct": true,
+          "id": "c5529ac2e01addeb1b1a5afbdbf9e29983ce1a93",
+          "message": "Skip unnecessary SPARQL SD requests for a single source\n\nCloses #1532",
+          "timestamp": "2025-04-08T16:01:48+02:00",
+          "tree_id": "66195ffce794efd1a413daba12ffe3586972b28d",
+          "url": "https://github.com/comunica/comunica/commit/c5529ac2e01addeb1b1a5afbdbf9e29983ce1a93"
+        },
+        "date": 1744380746965,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "WatDiv-File",
+            "value": 4924,
+            "unit": "ms"
+          },
+          {
+            "name": "WatDiv-TPF",
+            "value": 24149,
+            "unit": "ms"
+          },
+          {
+            "name": "BSBM-File",
+            "value": 384,
+            "unit": "ms"
+          },
+          {
+            "name": "BSBM-TPF",
+            "value": 1395,
+            "unit": "ms"
+          },
+          {
+            "name": "Web",
+            "value": 135295,
+            "unit": "ms"
+          }
+        ]
+      }
+    ]
+  }
+}
