389ee3764161ea3841dbcf3afe914d9da23d03bb
github-action-benchmark <github@users.noreply.github.com>
2025-04-14 14:17:33 +0000

    add Benchmarks total results (customSmallerIsBetter) benchmark result for 577fd93aa7d0f8288040d859b73133ceb8dac056

--- comunica/feature/join-sort-selectivity/benchmarks-total/data.js
+++ comunica/feature/join-sort-selectivity/benchmarks-total/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1744380747471,
+  "lastUpdate": 1744640252942,
   "repoUrl": "https://github.com/comunica/comunica",
   "entries": {
     "Benchmarks total results": [
@@ -48,6 +48,55 @@ window.BENCHMARK_DATA = {
           {
             "name": "Web",
             "value": 135295,
+            "unit": "ms"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "ruben.taelman@ugent.be",
+            "name": "Ruben Taelman",
+            "username": "rubensworks"
+          },
+          "committer": {
+            "email": "ruben.taelman@ugent.be",
+            "name": "Ruben Taelman",
+            "username": "rubensworks"
+          },
+          "distinct": true,
+          "id": "577fd93aa7d0f8288040d859b73133ceb8dac056",
+          "message": "Add join sorter based on join selectivities\n\nThis will be used as a fallback to the default cardinality-based join\nentry sorter, in case not all cardinalities are present.\n\nFor example, when the engine is unable to provide cardinalities for\ntriple patterns on SPARQL endpoints (they are all infinity), this new\nactor will provide a fallback approach to order them based on a variable\ncounting approach.\nThis corresponds to the default FedX approach.",
+          "timestamp": "2025-04-14T15:45:52+02:00",
+          "tree_id": "2d43bd88dbd5f4e15b561a2a9311810964fdb381",
+          "url": "https://github.com/comunica/comunica/commit/577fd93aa7d0f8288040d859b73133ceb8dac056"
+        },
+        "date": 1744640252427,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "WatDiv-File",
+            "value": 5748,
+            "unit": "ms"
+          },
+          {
+            "name": "WatDiv-TPF",
+            "value": 25647,
+            "unit": "ms"
+          },
+          {
+            "name": "BSBM-File",
+            "value": 341,
+            "unit": "ms"
+          },
+          {
+            "name": "BSBM-TPF",
+            "value": 1372,
+            "unit": "ms"
+          },
+          {
+            "name": "Web",
+            "value": 179942,
             "unit": "ms"
           }
         ]
