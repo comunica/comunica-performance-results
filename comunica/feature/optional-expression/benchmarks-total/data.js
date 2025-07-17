3220ed715401b09aa101637a462638cf4f1c4f6e
github-action-benchmark <github@users.noreply.github.com>
2025-07-17 07:22:42 +0000

    add Benchmarks total results (customSmallerIsBetter) benchmark result for 5a945ed4c24e8b9f16fe871d57f9c2d91f0e40e3

---
+++ comunica/feature/optional-expression/benchmarks-total/data.js
@@ -0,0 +1,57 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1752736962408,
+  "repoUrl": "https://github.com/comunica/comunica",
+  "entries": {
+    "Benchmarks total results": [
+      {
+        "commit": {
+          "author": {
+            "email": "ruben.taelman@ugent.be",
+            "name": "Ruben Taelman",
+            "username": "rubensworks"
+          },
+          "committer": {
+            "email": "ruben.taelman@ugent.be",
+            "name": "Ruben Taelman",
+            "username": "rubensworks"
+          },
+          "distinct": true,
+          "id": "5a945ed4c24e8b9f16fe871d57f9c2d91f0e40e3",
+          "message": "Fix incorrect environment command in issue template",
+          "timestamp": "2025-07-16T09:19:56+02:00",
+          "tree_id": "8a117aa238d61a8b31ed5bc2938eec5435be48ea",
+          "url": "https://github.com/comunica/comunica/commit/5a945ed4c24e8b9f16fe871d57f9c2d91f0e40e3"
+        },
+        "date": 1752736961420,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "WatDiv-File",
+            "value": 4075,
+            "unit": "ms"
+          },
+          {
+            "name": "WatDiv-TPF",
+            "value": 26397,
+            "unit": "ms"
+          },
+          {
+            "name": "BSBM-File",
+            "value": 336,
+            "unit": "ms"
+          },
+          {
+            "name": "BSBM-TPF",
+            "value": 1475,
+            "unit": "ms"
+          },
+          {
+            "name": "Web",
+            "value": 197524,
+            "unit": "ms"
+          }
+        ]
+      }
+    ]
+  }
+}
