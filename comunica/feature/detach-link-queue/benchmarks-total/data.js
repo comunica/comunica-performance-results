eda75492029637b1113dfd7e6e23e3a7227aa6b9
github-action-benchmark <github@users.noreply.github.com>
2025-10-16 06:02:07 +0000

    add Benchmarks total results (customSmallerIsBetter) benchmark result for 88b4689c509f390cb343071b2ec7cbc5c6b3e6ea

--- comunica/feature/detach-link-queue/benchmarks-total/data.js
+++ comunica/feature/detach-link-queue/benchmarks-total/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1759833995819,
+  "lastUpdate": 1760594527397,
   "repoUrl": "https://github.com/comunica/comunica",
   "entries": {
     "Benchmarks total results": [
@@ -48,6 +48,55 @@ window.BENCHMARK_DATA = {
           {
             "name": "Web",
             "value": 160024,
+            "unit": "ms"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "ruben.taelman@ugent.be",
+            "name": "Ruben Taelman",
+            "username": "rubensworks"
+          },
+          "committer": {
+            "email": "ruben.taelman@ugent.be",
+            "name": "Ruben Taelman",
+            "username": "rubensworks"
+          },
+          "distinct": true,
+          "id": "88b4689c509f390cb343071b2ec7cbc5c6b3e6ea",
+          "message": "Merge remote-tracking branch 'origin/master' into feature/detach-link-queue",
+          "timestamp": "2025-10-16T07:47:42+02:00",
+          "tree_id": "fd8ccead5232ea05ed8a2e156c00ca130bb22e44",
+          "url": "https://github.com/comunica/comunica/commit/88b4689c509f390cb343071b2ec7cbc5c6b3e6ea"
+        },
+        "date": 1760594526413,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "WatDiv-File",
+            "value": 3994,
+            "unit": "ms"
+          },
+          {
+            "name": "WatDiv-TPF",
+            "value": 22494,
+            "unit": "ms"
+          },
+          {
+            "name": "BSBM-File",
+            "value": 322,
+            "unit": "ms"
+          },
+          {
+            "name": "BSBM-TPF",
+            "value": 1332,
+            "unit": "ms"
+          },
+          {
+            "name": "Web",
+            "value": 117895,
             "unit": "ms"
           }
         ]
