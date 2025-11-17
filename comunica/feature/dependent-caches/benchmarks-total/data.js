02486fe8a71bc7554d37ca15370507c8e970ca96
github-action-benchmark <github@users.noreply.github.com>
2025-11-17 10:49:39 +0000

    add Benchmarks total results (customSmallerIsBetter) benchmark result for b8a20aa3c3e053fad59c1c4c2ded12227c64a0dc

--- comunica/feature/dependent-caches/benchmarks-total/data.js
+++ comunica/feature/dependent-caches/benchmarks-total/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1763131144628,
+  "lastUpdate": 1763376579278,
   "repoUrl": "https://github.com/comunica/comunica",
   "entries": {
     "Benchmarks total results": [
@@ -97,6 +97,55 @@ window.BENCHMARK_DATA = {
           {
             "name": "Web",
             "value": 179949,
+            "unit": "ms"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "ruben.taelman@ugent.be",
+            "name": "Ruben Taelman",
+            "username": "rubensworks"
+          },
+          "committer": {
+            "email": "ruben.taelman@ugent.be",
+            "name": "Ruben Taelman",
+            "username": "rubensworks"
+          },
+          "distinct": true,
+          "id": "b8a20aa3c3e053fad59c1c4c2ded12227c64a0dc",
+          "message": "Make caches depend on HTTP cache semantics\n\nThis exposes a cache policy on HTTP bus responses, and buses that rely\non it. While query sources and destinations were already being cached,\nthey were cached indefinitely. With this change, they now properly take\ninto account HTTP caching semantics, and become invalidated once the\nunderlying HTTP response becomes invalid.\n\nCurrently, this has one minor limitation.\nWe're not sending re-validation requests. So if the server sends a 304,\nwe will perform a new request and re-index the source. But if an\nHTTP-level cache is active, the actual HTTP request will not be sent, so\nonly local re-indexing will happen, which is negligible in most cases.\n\nPartially closes #166",
+          "timestamp": "2025-11-17T11:30:10+01:00",
+          "tree_id": "1900b85d9081caac2f1bf27ed27f76996165b9c5",
+          "url": "https://github.com/comunica/comunica/commit/b8a20aa3c3e053fad59c1c4c2ded12227c64a0dc"
+        },
+        "date": 1763376578209,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "WatDiv-File",
+            "value": 6425,
+            "unit": "ms"
+          },
+          {
+            "name": "WatDiv-TPF",
+            "value": 23672,
+            "unit": "ms"
+          },
+          {
+            "name": "BSBM-File",
+            "value": 361,
+            "unit": "ms"
+          },
+          {
+            "name": "BSBM-TPF",
+            "value": 1578,
+            "unit": "ms"
+          },
+          {
+            "name": "Web",
+            "value": 201150,
             "unit": "ms"
           }
         ]
