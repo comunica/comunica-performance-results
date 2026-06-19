a07e5211d3765101ff4a8595345ff53b3ae0f3ff
github-action-benchmark <github@users.noreply.github.com>
2026-06-19 11:11:46 +0000

    add Benchmarks total results (customSmallerIsBetter) benchmark result for 8d9b04741538acc2ead4a47830b4e48adbe4b90c

--- comunica/master/benchmarks-total/data.js
+++ comunica/master/benchmarks-total/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1781839823616,
+  "lastUpdate": 1781867506417,
   "repoUrl": "https://github.com/comunica/comunica",
   "entries": {
     "Benchmarks total results": [
@@ -10579,6 +10579,55 @@ window.BENCHMARK_DATA = {
           {
             "name": "Web",
             "value": 74415,
+            "unit": "ms"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "ruben.taelman@ugent.be",
+            "name": "Ruben Taelman",
+            "username": "rubensworks"
+          },
+          "committer": {
+            "email": "ruben.taelman@ugent.be",
+            "name": "Ruben Taelman",
+            "username": "rubensworks"
+          },
+          "distinct": true,
+          "id": "8d9b04741538acc2ead4a47830b4e48adbe4b90c",
+          "message": "Bump to release version v5.2.4",
+          "timestamp": "2026-06-19T12:48:38+02:00",
+          "tree_id": "5827089aa16386ad0f8c553df1adab2e1532bd7e",
+          "url": "https://github.com/comunica/comunica/commit/8d9b04741538acc2ead4a47830b4e48adbe4b90c"
+        },
+        "date": 1781867504812,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "WatDiv-File",
+            "value": 5202,
+            "unit": "ms"
+          },
+          {
+            "name": "WatDiv-TPF",
+            "value": 23138,
+            "unit": "ms"
+          },
+          {
+            "name": "BSBM-File",
+            "value": 322,
+            "unit": "ms"
+          },
+          {
+            "name": "BSBM-TPF",
+            "value": 1579,
+            "unit": "ms"
+          },
+          {
+            "name": "Web",
+            "value": 137697,
             "unit": "ms"
           }
         ]
