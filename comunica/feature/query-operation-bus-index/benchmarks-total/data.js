99c2030fd1c0917f2b2fce84d590274a572a999d
github-action-benchmark <github@users.noreply.github.com>
2026-08-23 06:49:38 +0000

    add Benchmarks total results (customSmallerIsBetter) benchmark result for fab8918b0fcca0606bf3d5437fd201029ba30586

---
+++ comunica/feature/query-operation-bus-index/benchmarks-total/data.js
@@ -0,0 +1,57 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1787467778212,
+  "repoUrl": "https://github.com/comunica/comunica",
+  "entries": {
+    "Benchmarks total results": [
+      {
+        "commit": {
+          "author": {
+            "email": "ruben.taelman@ugent.be",
+            "name": "Ruben Taelman",
+            "username": "rubensworks"
+          },
+          "committer": {
+            "email": "ruben.taelman@ugent.be",
+            "name": "Ruben Taelman",
+            "username": "rubensworks"
+          },
+          "distinct": true,
+          "id": "fab8918b0fcca0606bf3d5437fd201029ba30586",
+          "message": "Generate EARL test reports for different specs",
+          "timestamp": "2026-08-21T17:41:36+02:00",
+          "tree_id": "aac254f452f82af782143dfecd05861337d5d607",
+          "url": "https://github.com/comunica/comunica/commit/fab8918b0fcca0606bf3d5437fd201029ba30586"
+        },
+        "date": 1787467776833,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "WatDiv-File",
+            "value": 3609,
+            "unit": "ms"
+          },
+          {
+            "name": "WatDiv-TPF",
+            "value": 19525,
+            "unit": "ms"
+          },
+          {
+            "name": "BSBM-File",
+            "value": 329,
+            "unit": "ms"
+          },
+          {
+            "name": "BSBM-TPF",
+            "value": 1522,
+            "unit": "ms"
+          },
+          {
+            "name": "Web",
+            "value": 100886,
+            "unit": "ms"
+          }
+        ]
+      }
+    ]
+  }
+}
