6591f14eb1e902e6c20221feaf34c501ec1d231b
github-action-benchmark <github@users.noreply.github.com>
2026-08-23 09:34:39 +0000

    add Benchmarks total results (customSmallerIsBetter) benchmark result for fab8918b0fcca0606bf3d5437fd201029ba30586

--- comunica/feature/query-operation-bus-index/benchmarks-total/data.js
+++ comunica/feature/query-operation-bus-index/benchmarks-total/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1787476009059,
+  "lastUpdate": 1787477678895,
   "repoUrl": "https://github.com/comunica/comunica",
   "entries": {
     "Benchmarks total results": [
@@ -293,6 +293,55 @@ window.BENCHMARK_DATA = {
           {
             "name": "Web",
             "value": 89990,
+            "unit": "ms"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "ruben.taelman@ugent.be",
+            "name": "Ruben Taelman",
+            "username": "rubensworks"
+          },
+          "committer": {
+            "email": "ruben.taelman@ugent.be",
+            "name": "Ruben Taelman",
+            "username": "rubensworks"
+          },
+          "distinct": true,
+          "id": "fab8918b0fcca0606bf3d5437fd201029ba30586",
+          "message": "Generate EARL test reports for different specs",
+          "timestamp": "2026-08-21T17:41:36+02:00",
+          "tree_id": "aac254f452f82af782143dfecd05861337d5d607",
+          "url": "https://github.com/comunica/comunica/commit/fab8918b0fcca0606bf3d5437fd201029ba30586"
+        },
+        "date": 1787477677577,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "WatDiv-File",
+            "value": 5360,
+            "unit": "ms"
+          },
+          {
+            "name": "WatDiv-TPF",
+            "value": 23958,
+            "unit": "ms"
+          },
+          {
+            "name": "BSBM-File",
+            "value": 331,
+            "unit": "ms"
+          },
+          {
+            "name": "BSBM-TPF",
+            "value": 976,
+            "unit": "ms"
+          },
+          {
+            "name": "Web",
+            "value": 139123,
             "unit": "ms"
           }
         ]
